01b40c1ea8272d6999da916418dcb8647a3ba789
Mihail Dorozhko <dominick.alone@gmail.com>
2021-09-04 15:47:18 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,19 @@
 class GuessingGame {
-    constructor() {}
-
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.half = Math.round((this.min + this.max) / 2);
     }
 
     lower() {
-
+        return this.max = this.half;
     }
 
     greater() {
-
+        return this.min = this.half;
     }
 }
 
